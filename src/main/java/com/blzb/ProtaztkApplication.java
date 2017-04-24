98b9afea8f56be010e16f15d71603a6796c490d1
Angel Pimentel <angelpmza@gmail.com>
2017-04-23 21:47:16 -0500

    Initial commit

---
+++ src/main/java/com/blzb/ProtaztkApplication.java
@@ -0,0 +1,12 @@
+package com.blzb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProtaztkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProtaztkApplication.class, args);
+	}
+}
